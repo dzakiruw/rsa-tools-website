13f5306e9165514f1f74840518ac1695465ea068
dzakiruw <dzakiruw@gmail.com>
2023-03-30 08:49:36 +0700

    add feat: explanation

--- app/Http/Controllers/EncryptionController.php
+++ app/Http/Controllers/EncryptionController.php
@@ -71,30 +71,36 @@ class EncryptionController extends Controller
             'plaintext' => $plaintext,
         ]);
     }
+    //Mendefinisikan fungsi publik gcd untuk menghitung FPB (Faktor Persekutuan Terbesar) dari dua bilangan $a dan $b.
     public function gcd($a, $b)
     {
+        //Jika $a sama dengan 0, maka kembalikan nilai $b. Jika tidak, panggil fungsi gcd dengan argumen ($b % $a, $a).
         if ($a == 0)
             return $b;
         return $this->gcd($b % $a, $a);
     }
 
+    //Mendefinisikan fungsi publik modInverse untuk menghitung invers modulo dari bilangan $a modulo $m.
     public function modInverse($a, $m)
     {
+        //Lakukan perulangan dari 1 hingga $m - 1, jika ditemukan nilai $x yang memenuhi ($a * $x) % $m == 1, kembalikan nilai $x tersebut.
         for ($x = 1; $x < $m; $x++)
             if (($a * $x) % $m == 1)
                 return $x;
         return 1;
     }
 
+    //untuk menghasilkan pasangan kunci publik dan privat RSA berdasarkan dua bilangan prima $p dan $q.
     public function generateKeys($p, $q)
     {
         $n = $p * $q;
         $phi = ($p - 1) * ($q - 1);
 
+        //Cari nilai $e$ yang memenuhi FPB($e, \phi) = 1.
         for ($e = 2; $e < $phi; $e++)
             if ($this->gcd($e, $phi) == 1)
                 break;
-
+        //Menghitung nilai $d$ sebagai invers dari $e$ modulo $\phi$.
         $d = $this->modInverse($e, $phi);
 
         return [
@@ -109,7 +115,9 @@ class EncryptionController extends Controller
         $ciphertext = [];
 
         foreach (str_split($plaintext) as $char) {
+            //Mengubah karakter menjadi kode ASCII.
             $m = ord($char);
+            //Mengenkripsi karakter dengan rumus RSA $c = m^e \pmod{n}$.
             $ciphertext[] = bcpowmod($m, $e, $n);
         }
 
@@ -124,7 +132,9 @@ class EncryptionController extends Controller
         $ciphertext_array = explode(",", $ciphertext);
 
         foreach ($ciphertext_array as $cipher) {
+            //Mendekripsi elemen teks terenkripsi dengan rumus RSA $m = c^d \pmod{n}$.
             $m = bcpowmod($cipher, $d, $n);
+            //Mengubah kode ASCII kembali menjadi karakter dan menyambungkannya ke teks yang telah didekripsi.
             $plaintext .= chr($m);
         }
 
